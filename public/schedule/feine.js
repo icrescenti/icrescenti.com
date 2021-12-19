93e65ec885c4148d9096099e6ecf59447bc54ad6
icrescenti <icrescenti@terkstudios.com>
2021-12-19 23:03:32 +0100

    new

--- public/schedule/feine.js
+++
@@ -1,7 +0,0 @@
-var today = new Date();
-var d = String(today.getDate()).padStart(2, '0');
-var m = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var y = today.getFullYear();
-
-today = d + '-' + m + '-' + y;
-document.getElementById("dates").innerHTML = "21-09-2020/"+today;
