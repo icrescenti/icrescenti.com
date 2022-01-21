e2b9dfed7d0a62c1b87af58a2273eacabca3c63f
icrescenti <icrescenti@terkstudios.com>
2022-01-21 15:27:29 +0100

    update

--- public/nuts/code.js
+++ public/nuts/code.js
@@ -27,10 +27,10 @@ var orangeIcon = new L.Icon({
 var burgirIcon = new L.Icon({
   iconUrl: './img/burgir.png',
   shadowUrl: '',
-  iconSize: [30, 30],
+  iconSize: [24, 24],
   iconAnchor: [34, 20],
   popupAnchor: [10, -34],
-  shadowSize: [34, 34]
+  shadowSize: [24, 24]
 });
 
 var jackIcon = new L.Icon({
@@ -41,43 +41,165 @@ var jackIcon = new L.Icon({
   shadowSize: [54, 54]
 });
 
+////////////////////////////////////////
+
+var foods = []
+var places = []
+var halloween = []
+var not_confirmed = []
+var distances = []
+
+
+handlePlace()
+handleFood()
+handleHalloween()
+handleUnconfirmed()
+handleDistance()
+
 /////////////////////////////////////////
+
+function handlePlace(){
+  if(document.querySelector('input[name="placeLegend"]').checked) {
+    //PLACES
+    afegir([41.9655403,2.7388837], "Bescanó");
+    afegir([34.0101225,-118.4962244], "Santa Monica Pier and Beach");
+    afegir([34.010522994422175, -118.49580552968682], "Starbuck Pier");
+    afegir([33.98547801667092, -118.47260031816063], "Muscle Beach");
+    afegir([34.101668967664395, -118.34462554204954], "Inici Fame Walk");
+    afegir([34.10172923061826, -118.3224437532545], "Final Fame Walk");
+    afegir([34.10115398836917, -118.34470198425869], "Bar Lucifer (Emerson Theatre)");
+    afegir([33.81469056831722, -117.92130531200917], "STAR WARS CHULI");
+    afegir([34.13419619144182, -118.3215586594631], "MYSTERY");
+    afegir([33.985404941031746, -118.47343585793575], "Squash Explosiu");
+    afegir([33.94449007374263, -118.40812799930345], "Airport");
+    afegir([41.30265812238073, 2.083713963630138], "El prat");
+  }
+  else
+    delPlace()
+}
 
 function afegir(p,n)
 {
-  L.marker(p)
+  places.push(L.marker(p)
   .bindTooltip(n)
-  .addTo(map);
+  .addTo(map))
+}
+
+function delPlace() {
+  places.map(place => map.removeLayer(place))
+}
+
+function handleFood() {
+  if(document.querySelector('input[name="foodLegend"]').checked) {
+    //MENJARS
+    afegirFood([34.04710261584794, -118.34029518897364], "Five Guys #1");
+    afegirFood([34.06219169923002, -118.3499037190155], "Five Guys #2");
+  }
+  else
+    delFood()
 }
 
 function afegirFood(p,n)
 {
-  L.marker(p, {
+  foods.push(L.marker(p, {
     icon: burgirIcon
   })
-  .bindTooltip(n).addTo(map);
+  .bindTooltip(n).addTo(map))
+}
+
+function delFood() {
+  foods.map(food => map.removeLayer(food))
+}
+
+function handleHalloween(){
+  if(document.querySelector('input[name="halloweenLegend"]').checked) {
+    //HALLOWEEN
+    afegirHalloween([34.13821011592698, -118.35338088084781], "Universal Studios");
+    afegirHalloween([34.0501312549111, -118.31734168256287], "Murder House (AHS)");
+    afegirHalloween([33.81224616157229, -117.91896519629202], "Disneyland (miki calabaza)");
+    afegirHalloween([34.20153432221186, -118.21121786121351], "Parque calabazas");
+  }
+  else
+    delHalloween()
 }
 
 function afegirHalloween(p,n)
 {
-  L.marker(p, {
+  halloween.push(L.marker(p, {
     icon: jackIcon
   })
-  .bindTooltip(n).addTo(map);
+  .bindTooltip(n).addTo(map))
+}
+
+function delHalloween() {
+  halloween.map(halloween_ => map.removeLayer(halloween_))
+}
+
+function handleUnconfirmed() {
+  if(document.querySelector('input[name="unconfirmedLegend"]').checked) {
+    //UNCONFIRMED
+    want_to_location([34.00853260984171, -118.49697447127076], "Lucifer S02E18 33:50");
+    want_to_location([34.00815152328652, -118.49652177931772], "Baywatch (1989) tower");
+    want_to_location([34.11909217762249, -118.3003768709492], "Observatori");
+    want_to_location([34.09088395079369, -118.39399287613533], "Eclipse Tower");
+    want_to_location([34.090525183180056, -118.39208778550851], "Eclipse Tower (Take photo)");
+    want_to_location([34.076091455302084, -118.42950848465397], "Playboy mansion");
+  }
+  else
+    delUnconfirmed()
 }
 
 function want_to_location(p,n)
 {
-  L.marker(p, {
+  not_confirmed.push(L.marker(p, {
     icon: orangeIcon
   })
-  .bindTooltip(n).addTo(map);
+  .bindTooltip(n).addTo(map))
+}
+
+function delUnconfirmed() {
+  not_confirmed.map(not_confirmed_ => map.removeLayer(not_confirmed_))
+}
+
+function handleDistance() {
+  if(document.querySelector('input[name="distanceLegend"]').checked) {
+    //DISTANCIES
+    addDistance([
+      [34.13821011592698, -118.35338088084781],
+      [34.13419619144182, -118.3215586594631]
+    ],
+    "?")
+
+    addDistance([
+      [34.010522994422175, -118.49580552968682],
+      [33.98547801667092, -118.47260031816063]
+    ],
+    "3,7 KM (🚶44m, 🚗7m)")
+
+    addDistance([
+      [34.101668967664395, -118.34462554204954],
+      [34.10172923061826, -118.3224437532545]
+    ],
+    "2 KM (🚶?m, 🚗6m)")
+
+    addDistance([
+      [34.101668967664395, -118.34462554204954],
+      [34.10115398836917, -118.34470198425869]
+    ],
+    "100 metres (🚶1m)")
+  }
+  else
+    delDistances()
 }
 
 function addDistance(points, distance) {
-  L.polyline(points)
+  distances.push(L.polyline(points)
   .bindTooltip(distance)
-  .addTo(map);
+  .addTo(map))
+}
+
+function delDistances() {
+  distances.map(distance => map.removeLayer(distance))
 }
 
 function flight(points, distance) {
@@ -95,62 +217,6 @@ function flight(points, distance) {
 
   polylineMeasure.seed([points])
 }
-
-afegir([41.9655403,2.7388837], "Bescanó");
-afegir([34.0101225,-118.4962244], "Santa Monica Pier and Beach");
-afegir([34.010522994422175, -118.49580552968682], "Starbuck Pier");
-afegir([33.98547801667092, -118.47260031816063], "Muscle Beach");
-want_to_location([34.00853260984171, -118.49697447127076], "Lucifer S02E18 33:50");
-afegir([34.101668967664395, -118.34462554204954], "Inici Fame Walk");
-afegir([34.10172923061826, -118.3224437532545], "Final Fame Walk");
-afegir([34.10115398836917, -118.34470198425869], "Bar Lucifer (Emerson Theatre)");
-afegir([33.81469056831722, -117.92130531200917], "STAR WARS CHULI");
-afegir([34.13419619144182, -118.3215586594631], "MYSTERY");
-want_to_location([34.00815152328652, -118.49652177931772], "Baywatch (1989) tower");
-afegir([33.985404941031746, -118.47343585793575], "Squash Explosiu");
-afegir([33.94449007374263, -118.40812799930345], "Airport");
-afegir([41.30265812238073, 2.083713963630138], "El prat");
-want_to_location([34.11909217762249, -118.3003768709492], "Observatori");
-want_to_location([34.09088395079369, -118.39399287613533], "Eclipse Tower");
-want_to_location([34.090525183180056, -118.39208778550851], "Eclipse Tower (Take photo)");
-want_to_location([34.076091455302084, -118.42950848465397], "Playboy mansion");
-
-//HALLOWEEN
-
-afegirHalloween([34.13821011592698, -118.35338088084781], "Universal Studios");
-afegirHalloween([34.0501312549111, -118.31734168256287], "Murder House (AHS)");
-afegirHalloween([33.81224616157229, -117.91896519629202], "Disneyland (miki calabaza)");
-afegirHalloween([34.20153432221186, -118.21121786121351], "Parque calabazas");
-
-//MENJARS
-afegirFood([34.04710261584794, -118.34029518897364], "Five Guys");
-afegirFood([34.06219169923002, -118.3499037190155], "Five Guys");
-
-//DISTANCIES
-
-addDistance([
-  [34.13821011592698, -118.35338088084781],
-  [34.13419619144182, -118.3215586594631]
-],
-"?")
-
-addDistance([
-  [34.010522994422175, -118.49580552968682],
-  [33.98547801667092, -118.47260031816063]
-],
-"3,7 KM (🚶44m, 🚗7m)")
-
-addDistance([
-  [34.101668967664395, -118.34462554204954],
-  [34.10172923061826, -118.3224437532545]
-],
-"2 KM (🚶?m, 🚗6m)")
-
-addDistance([
-  [34.101668967664395, -118.34462554204954],
-  [34.10115398836917, -118.34470198425869]
-],
-"100 metres (🚶1m)")
 
 
 /*
